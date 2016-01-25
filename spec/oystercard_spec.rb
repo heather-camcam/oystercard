0588c432d3d389cfe1ed14b10f5808de06eb999e
Mic Cassano <mic.cassano@gmail.com>
2016-01-25 16:03:49 +0000

    Implemenet and test Oystercard#top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,20 @@
 require 'oystercard'
 
 describe Oystercard do
-
   subject(:oystercard) { described_class.new }
 
   describe '#initialize' do
-
     it 'initializes with a balance of 0' do
       expect(oystercard.balance).to eq(0)
     end
-
   end
 
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'adds money to card balance' do
+      oystercard.top_up 10
+      expect(oystercard.balance).to eq 10
+    end
+  end
 end
